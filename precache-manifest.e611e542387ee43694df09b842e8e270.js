795ee7d4a4cd73263c7101d176055f68bfb031e5
VatsalGujarati <vatsal1568@gmail.com>
2020-10-10 00:47:25 +0530

    Updates

---
+++ precache-manifest.e611e542387ee43694df09b842e8e270.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e35bf339e8d0f3e36aeffaa3ff4b8ed",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/my-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f8432ed11fd1f7d1eafc",
+    "url": "/my-app/static/css/main.322c728e.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/my-app/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8432ed11fd1f7d1eafc",
+    "url": "/my-app/static/js/main.19a6df73.chunk.js"
+  },
+  {
+    "revision": "c2a1dcc6438bccc67aae",
+    "url": "/my-app/static/js/runtime-main.813cd07b.js"
+  }
+]);
